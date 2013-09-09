71044abdff8c3b7d9e7a57421178cd7f6b2d740d
Andre <aforesti@gmail.com>
2013-09-09 21:03:08 +0000

    app criada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinanceOnRails::Application.config.secret_key_base = 'f6508be09dea3f955701627ad64ccdedd59702da7c27df7d6039adf30e2927700412993c6608d9717cd2e6220fe5f347c7a72d7ab76a6bf3d2c988874da28955'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FinanceOnRails::Application.config.session_store :cookie_store, key: '_finance_on_rails_session'
